26c5e36a6613bc8c82ee3b69968b0050c5702bc3
W1W1-M <w1w1_m@icloud.com>
2022-05-30 21:43:17 +0200

    Add UI

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount: Double = 0
+    @State private var totalSplits: Int = 0
+    @State private var tipPart: Int = 0
+    @State private var shareAmount: Double = 0
+    let tip: Array<Int> = [0, 10, 15, 20, 30]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                        .keyboardType(.decimalPad)
+                    Picker("People", selection: $totalSplits) {
+                        ForEach(2..<13) {
+                            Text("\($0) people").tag($0)
+                        }
+                    }.pickerStyle(.menu)
+                } header: {
+                    Text("Check to split")
+                }
+                Section {
+                    Picker("tip", selection: $tipPart) {
+                        ForEach(tip, id: \.self) {
+                            Text($0, format: .percent).tag($0)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("Select tip")
+                }
+                Section {
+                    Text(shareAmount, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                } header: {
+                    Text("Share per person")
+                }
+            }.navigationTitle("WeSplit")
+        }
     }
 }
 
